ccd3b56f4daf6c58b16b00b4989caeb2bcaab4a6
diyan26 <yandyn095@gmail.com>
2025-10-26 16:35:24 +0700

    fix: auth persistence for chat system

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,23 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  setPersistence,
+  browserLocalPersistence,
+  signOut,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyBoJrSQysYB-H7I0AIv8Dh_Uzh6yq54Oz8",
+  apiKey: "AIzaSyBoJrSQysYB-H7I0AIv8Dh_Uzh6yq54Oz8",
   authDomain: "diyan-portfolio.firebaseapp.com",
   projectId: "diyan-portfolio",
   storageBucket: "diyan-portfolio.firebasestorage.app",
   messagingSenderId: "364471384503",
   appId: "1:364471384503:web:02553f445073328a5d1032",
-  measurementId: "G-SNCG3RGEJ1"
+  measurementId: "G-SNCG3RGEJ1",
 };
 
 // Init Firebase
